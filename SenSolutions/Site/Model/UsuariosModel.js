d18aa5253cc093eeb274354846149858c3b4e7e7
fernandoguide <fernando-guide@hotmail.com>
2019-05-23 22:35:00 -0300

    algumas mudancas

--- SenSolutions/Site/Model/UsuariosModel.js
+++ SenSolutions/Site/Model/UsuariosModel.js
@@ -7,22 +7,22 @@ module.exports = class UsuariosModel{
     }
 
     static getId(id, callback) {
-        return db.query("SELECT * FROM tb_cliente WHERE idCliente=?", [idcliente], callback);
+        return db.query("SELECT * FROM tb_cliente WHERE idCliente=?", [id], callback);
     }
 
-    static adicionar(portfolio, callback) {
+    static adicionar(tb_cliente, callback) {
 
-        return db.query("INSERT INTO tb_cliente (descricao, detalhes) VALUES(?,?)", 
-        [portfolio.descricao, portfolio.detalhes], callback);        
+        return db.query("INSERT INTO tb_cliente (nomeUsuario, senhaUsuario,Email) VALUES(?,?)",
+        [tb_cliente.nomeUsuario, tb_cliente.senhaUsuario,tb_cliente.Email], callback);
     }
 
     static deletar(id, callback) {
         return db.query("DELETE FROM tb_cliente WHERE  idCliente=?", [id], callback);
     }
 
-    static editar(portfolio, callback) {
-        return db.query("UPDATE tb_cliente SET descricao=?, detalhes=? WHERE  idCliente=?", 
-        [portfolio.descricao, portfolio.detalhes, portfolio.id_portfolio], callback); 
+    static editar(tb_cliente, callback) {
+        return db.query("UPDATE tb_cliente SET nomeUsuario=?, senhaUsuario=?,Email=? WHERE  idCliente=?",
+        [tb_cliente.nomeUsuario, tb_cliente.senhaUsuario, tb_cliente.Email, tb_cliente.id_portfolio], callback);
     }
 
 };

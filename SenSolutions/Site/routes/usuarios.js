f8153197bde3f09e8a6869bb4d264e34f760a125
fernandoguide <fernando-guide@hotmail.com>
2019-05-23 22:35:58 -0300

    att

--- SenSolutions/Site/routes/usuarios.js
+++ SenSolutions/Site/routes/usuarios.js
@@ -5,8 +5,8 @@ var banco = require('../app-banco');
 // não mexa nessas 3 linhas!
 
 
-var UsuariosModel = require('../model/UsuariosModel');
-var RespostaClass = require('../Model/RespostaClass');
+// var UsuariosModel = require('../model/UsuariosModel');
+// var RespostaClass = require('../Model/RespostaClass');
 
 router.post('/entrar', function (req, res, next) {
 
@@ -44,111 +44,111 @@ router.post('/entrar', function (req, res, next) {
 
 
 
-router.get('/', function (req, res, next) {
+// router.get('/', function (req, res, next) {
 
-    UsuariosModel.getTodos(function (erro, retorno) {
+//     UsuariosModel.getTodos(function (erro, retorno) {
 
-        let resposta = new RespostaClass();
-        if (erro) {
-            resposta.erro = true;
-            resposta.msg = "Ocorreu um erro.";
-            console.log("erro", erro);
-        }
-        else resposta.dados = retorno;
-        res.json(resposta);
-    });
-});
-
-
-router.get('/:id?', function (req, res, next) {
-
-    UsuariosModel.getId(req.params.id, function (erro, retorno) {
-
-        let resposta = new RespostaClass();
-
-        if (erro) {
-            resposta.erro = true;
-            resposta.msg = "Ocorreu um erro.";
-            console.log("erro", erro);
-        }
-        else
-            resposta.dados = retorno;
-        res.json(resposta);
-    });
-});
-
-router.post('/', function (req, res, next) {
-
-    UsuariosModel.adicionar(req.body, function (erro, retorno) {
-
-        let resposta = new RespostaClass();
-
-        if (erro) {
-            resposta.erro = true;
-            resposta.msg = "Ocorreu um erro.";
-            console.log("erro", erro);
-        }
-        else {
-            if (retorno.affectedRows > 0) {
-                resposta.msg = "cadastro realizado com sucesso";
-            } else {
-                resposta.erro = true;
-                resposta.msg = "Não foi realziar o cadastro";
-            }
-        }
-        console.log('resp cadastrar', resposta);
-        res.json(resposta);
-    });
-});
+//         let resposta = new RespostaClass();
+//         if (erro) {
+//             resposta.erro = true;
+//             resposta.msg = "Ocorreu um erro.";
+//             console.log("erro", erro);
+//         }
+//         else resposta.dados = retorno;
+//         res.json(resposta);
+//     });
+// });
 
 
+// router.get('/:id?', function (req, res, next) {
 
-router.delete('/:id', function (req, res, next) {
+//     UsuariosModel.getId(req.params.id, function (erro, retorno) {
 
-    UsuariosModel.deletar(req.params.id, function (erro, retorno) {
+//         let resposta = new RespostaClass();
 
-        let resposta = new RespostaClass();
+//         if (erro) {
+//             resposta.erro = true;
+//             resposta.msg = "Ocorreu um erro.";
+//             console.log("erro", erro);
+//         }
+//         else
+//             resposta.dados = retorno;
+//         res.json(resposta);
+//     });
+// });
 
-        if (erro) {
-            resposta.erro = true;
-            resposta.msg = "Ocorreu um erro.";
-            console.log("erro", erro);
-        }
-        else {
-            if (retorno.affectedRows > 0) {
-                resposta.msg = "registro excluído com sucesso";
-            } else {
-                resposta.erro = true;
-                resposta.msg = "Não foi possível excluir o registro";
-            }
-        }
-        res.json(resposta);
-    });
-});
+// router.post('/', function (req, res, next) {
 
-router.put('/', function (req, res, next) {
+//     UsuariosModel.adicionar(req.body, function (erro, retorno) {
 
-    UsuariosModel.editar(req.body, function (erro, retorno) {
+//         let resposta = new RespostaClass();
 
-        let resposta = new RespostaClass();
+//         if (erro) {
+//             resposta.erro = true;
+//             resposta.msg = "Ocorreu um erro.";
+//             console.log("erro", erro);
+//         }
+//         else {
+//             if (retorno.affectedRows > 0) {
+//                 resposta.msg = "cadastro realizado com sucesso";
+//             } else {
+//                 resposta.erro = true;
+//                 resposta.msg = "Não foi realziar o cadastro";
+//             }
+//         }
+//         console.log('resp cadastrar', resposta);
+//         res.json(resposta);
+//     });
+// });
 
-        if (erro) {
-            resposta.erro = true;
-            resposta.msg = "Ocorreu um erro.";
-            console.log("erro", erro);
-        }
-        else {
-            if (retorno.affectedRows > 0) {
-                resposta.msg = "registro alterado com sucesso";
-            } else {
-                resposta.erro = true;
-                resposta.msg = "Não foi possível alterar o registro";
-            }
-        }
-        res.json(resposta);
-    });
 
-});
+
+// router.delete('/:id', function (req, res, next) {
+
+//     UsuariosModel.deletar(req.params.id, function (erro, retorno) {
+
+//         let resposta = new RespostaClass();
+
+//         if (erro) {
+//             resposta.erro = true;
+//             resposta.msg = "Ocorreu um erro.";
+//             console.log("erro", erro);
+//         }
+//         else {
+//             if (retorno.affectedRows > 0) {
+//                 resposta.msg = "registro excluído com sucesso";
+//             } else {
+//                 resposta.erro = true;
+//                 resposta.msg = "Não foi possível excluir o registro";
+//             }
+//         }
+//         res.json(resposta);
+//     });
+// });
+
+// router.put('/', function (req, res, next) {
+
+//     UsuariosModel.editar(req.body, function (erro, retorno) {
+
+//         let resposta = new RespostaClass();
+
+//         if (erro) {
+//             resposta.erro = true;
+//             resposta.msg = "Ocorreu um erro.";
+//             console.log("erro", erro);
+//         }
+//         else {
+//             if (retorno.affectedRows > 0) {
+//                 resposta.msg = "registro alterado com sucesso";
+//             } else {
+//                 resposta.erro = true;
+//                 resposta.msg = "Não foi possível alterar o registro";
+//             }
+//         }
+//         res.json(resposta);
+//     });
+
+// });
 
 
 // não mexa nesta linha!

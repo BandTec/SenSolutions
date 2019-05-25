7ace3f1d575dcce06a339410008f6b6290f1a2fc
LuisFrag <fragaaahdlc@gmail.com>
2019-05-25 15:23:15 -0300

    Separação do grafico de temperatura e umidade

--- SenSolutions/Site/public/js/graficos.js
+++ SenSolutions/Site/public/js/graficos.js
@@ -1,139 +1,268 @@
+var usuario;
+var exibiu_graficoTemp = false;
+var exibiu_graficoUmid = false;
+verificarAutenticacao();
 
-    var usuario;
-    var exibiu_grafico = false;
 
+function verificarAutenticacao() {
+    usuario = sessionStorage.usuario_bandtec;
+    if (usuario == undefined) {
+        window.location.href = 'login.html';
+    } else {
+        b_usuario.innerHTML = usuario;
+    }
+}
+
+function logoff() {
+    sessionStorage.removeItem('usuario_bandtec');
     verificarAutenticacao();
+}
 
+// só mexer se quiser alterar o tempo de atualização
+// ou se souber o que está fazendo!
+function atualizarGrafico() {
+    obterDadosGrafico();
+    setTimeout(atualizarGrafico, 10000);
+}
 
-    function verificarAutenticacao() {
-        usuario = sessionStorage.usuario_bandtec;
-        if (usuario == undefined) {
-            window.location.href = 'login.html';
-        } else {
-            b_usuario.innerHTML = usuario;
-        }
-    }
-
-    function logoff() {
-        sessionStorage.removeItem('usuario_bandtec');
-        verificarAutenticacao();
-    }
-
-    // só mexer se quiser alterar o tempo de atualização
-    // ou se souber o que está fazendo!
-    function atualizarGrafico() {
-        obterDadosGrafico();
-        setTimeout(atualizarGrafico, 10000);
-    }
-
-    // altere aqui as configurações do gráfico
-    // (tamanhos, cores, textos, etc)
-    function configurarGrafico() {
-        var configuracoes = {
-            responsive: true,
-            animation: exibiu_grafico ? false : { duration: 1500 },
-            hoverMode: 'index',
-            stacked: false,
-            title: {
+// altere aqui as configurações do gráfico
+// (tamanhos, cores, textos, etc)
+function configurarGraficoTemp() {
+    var configuracoes = {
+        responsive: true,
+        animation: exibiu_graficoTemp ? false : { duration: 1500 },
+        hoverMode: 'index',
+        stacked: false,
+        title: {
+            display: true,
+            text: 'Histórico recente de temperatura'
+        },
+        scales: {
+            yAxes: [{
+                type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
                 display: true,
-                text: 'Histórico recente de temperatura e umidade'
-            },
-            scales: {
-                yAxes: [{
-                    type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-                    display: true,
-                    position: 'left',
-                    id: 'y-temperatura',
-                }, {
-                    type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-                    display: true,
-                    position: 'right',
-                    id: 'y-umidade',
-
-                    // grid line settings
-                    gridLines: {
-                        drawOnChartArea: false, // only want the grid lines for one axis to show up
-                    },
-                }],
+                position: 'left',
+                id: 'y-temperatura',
             }
-        };
+        ],
+        }
+    };
 
-        exibiu_grafico = true;
+    exibiu_graficoTemp = true;
 
-        return configuracoes;
-    }
+    return configuracoes;
+}
 
-    // altere aqui como os dados serão exibidos
-    // e como são recuperados do BackEnd
-    function obterDadosGrafico() {
+// altere aqui as configurações do gráfico
+// (tamanhos, cores, textos, etc)
+function configurarGraficoUmid() {
+    var configuracoes = {
+        responsive: true,
+        animation: exibiu_graficoUmid ? false : { duration: 1500 },
+        hoverMode: 'index',
+        stacked: false,
+        title: {
+            display: true,
+            text: 'Histórico recente de umidade'
+        },
+        scales: {
+            yAxes: [{
+                type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                display: true,
+                position: 'left',
+                id: 'y-umidade',
+            }         
+        ],
+        }
+    };
 
-        // neste JSON tem que ser 'labels', 'datasets' etc, 
-        // porque é o padrão do Chart.js
-        var dados = {
-            labels: [],
-            datasets: [
-                {
-                    yAxisID: 'y-temperatura',
-                    label: 'Temperatura',
-                    borderColor: window.chartColors.red,
-                    backgroundColor: window.chartColors.red,
-                    fill: false,
-                    data: []
-                },
-                {
-                    yAxisID: 'y-umidade',
-                    label: 'Umidade',
-                    borderColor: window.chartColors.blue,
-                    backgroundColor: window.chartColors.blue,
-                    fill: false,
-                    data: []
+    exibiu_graficoUmid = true;
+
+    return configuracoes;
+}
+
+// altere aqui como os dados serão exibidos
+// e como são recuperados do BackEnd
+function obterDadosGrafico() {
+
+    // neste JSON tem que ser 'labels', 'datasets' etc, 
+    // porque é o padrão do Chart.js
+    
+    var dadosumid = {
+        labels:[],
+        layout: {
+            padding: {
+                left: 100,
+                right: 100,
+                top: 100,
+                bottom: 100
+            }
+        },
+        datasets: [
+            {
+                yAxisID: 'y-umidade',
+                label: 'Umidade',
+                borderColor:'#4B67C1' ,
+                backgroundColor: '#4B67C1',
+                fill: false,
+                data: []
+            }
+        ]
+    };
+
+    var dadostemp = {
+        labels: [],
+        datasets: [
+            {
+                yAxisID: 'y-temperatura',
+                label: 'Temperatura',
+                borderColor: '#C44848',
+                backgroundColor: '#C44848',
+                fill: false,
+                data: []
+            }          
+        ]
+    };
+
+    fetch('/leituras/ultimas', { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+
+                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+
+                resposta.reverse();
+
+                for (i = 0; i < resposta.length; i++) {
+                    var registro = resposta[i];
+
+                    // aqui, após 'registro.' use os nomes 
+                    // dos atributos que vem no JSON 
+                    // que gerou na consulta ao banco de dados
+
+                    dadostemp.labels.push(registro.dataHora);
+                    dadostemp.datasets[0].data.push(registro.temperatura);
+
+                    dadosumid.labels.push(registro.dataHora);
+                    dadosumid.datasets[0].data.push(registro.umidade);
                 }
-            ]
-        };
+                console.log(JSON.stringify(dadostemp,dadosumid));
 
-        fetch('/leituras/ultimas', { cache: 'no-store' }).then(function (response) {
-            if (response.ok) {
-                response.json().then(function (resposta) {
+                div_aguarde.style.display = 'none';
+                div_aguarde1.style.display = 'none';
 
-                    console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
-
-                    resposta.reverse();
-
-                    for (i = 0; i < resposta.length; i++) {
-                        var registro = resposta[i];
-
-                        // aqui, após 'registro.' use os nomes 
-                        // dos atributos que vem no JSON 
-                        // que gerou na consulta ao banco de dados
-
-                        dados.labels.push(registro.dataHora);
-
-                        dados.datasets[0].data.push(registro.temperatura);
-                        dados.datasets[1].data.push(registro.umidade);
-                    }
-                    console.log(JSON.stringify(dados));
-
-                    div_aguarde.style.display = 'none';
-
-                    plotarGrafico(dados);
-                });
-            } else {
-                console.error('Nenhum dado encontrado ou erro na API');
-            }
-        })
-            .catch(function (error) {
-                console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+                plotarGrafico(dadostemp,dadosumid);
             });
-
-    }
-
-    // só altere aqui se souber o que está fazendo!
-    function plotarGrafico(dados) {
-        console.log('iniciando plotagem do gráfico...');
-
-        var ctx = canvas_grafico.getContext('2d');
-        window.grafico_linha = Chart.Line(ctx, {
-            data: dados,
-            options: configurarGrafico()
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
         });
-    }
+}
+
+// altere aqui como os dados serão exibidos
+// e como são recuperados do BackEnd
+// function obterDadosGraficoUmid() {
+
+//     // neste JSON tem que ser 'labels', 'datasets' etc, 
+//     // porque é o padrão do Chart.js
+
+//     var dadosumid = {
+//         labels: [],
+//         datasets: [
+//             {
+//                 yAxisID: 'y-umidade',
+//                 label: 'Umidade',
+//                 borderColor: window.chartColors.blue,
+//                 backgroundColor: window.chartColors.blue,
+//                 fill: false,
+//                 data: []
+//             }
+//             // ,
+//             // {
+//             //     yAxisID: 'y-umidade',
+//             //     label: 'Umidade',
+//             //     borderColor: window.chartColors.blue,
+//             //     backgroundColor: window.chartColors.blue,
+//             //     fill: false,
+//             //     data: []
+//             // }
+//         ]
+//     };
+
+//     var dadostemp = {
+//         labels: [],
+//         datasets: [
+//             {
+//                 yAxisID: 'y-temperatura',
+//                 label: 'Temperatura',
+//                 borderColor: window.chartColors.red,
+//                 backgroundColor: window.chartColors.red,
+//                 fill: false,
+//                 data: []
+//             }
+//             // ,
+//             // {
+//             //     yAxisID: 'y-umidade',
+//             //     label: 'Umidade',
+//             //     borderColor: window.chartColors.blue,
+//             //     backgroundColor: window.chartColors.blue,
+//             //     fill: false,
+//             //     data: []
+//             // }
+//         ]
+//     };
+//     fetch('/leituras/ultimas', { cache: 'no-store' }).then(function (response) {
+//         if (response.ok) {
+//             response.json().then(function (resposta) {
+
+//                 console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+
+//                 resposta.reverse();
+
+//                 for (i = 0; i < resposta.length; i++) {
+//                     var registro = resposta[i];
+
+//                     // aqui, após 'registro.' use os nomes 
+//                     // dos atributos que vem no JSON 
+//                     // que gerou na consulta ao banco de dados
+//                     dadostemp.labels.push(registro.dataHora);
+//                     dadosumid.labels.push(registro.dataHora);
+//                     dadostemp.datasets[0].data.push(registro.temperatura);
+//                     dadosumid.datasets[0].data.push(registro.umidade);
+//                     // dados.datasets[1].data.push(registro.umidade);
+//                 }
+//                 console.log(JSON.stringify(dadostemp,dadosumid));
+
+//                 div_aguarde1.style.display = 'none';
+
+//                 plotarGrafico(dadosumid);
+//             });
+//         } else {
+//             console.error('Nenhum dado encontrado ou erro na API');
+//         }
+//     })
+//         .catch(function (error) {
+//             console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+//         });
+
+// }
+
+// só altere aqui se souber o que está fazendo!
+
+function plotarGrafico(dados,dadosumid) {
+    console.log('iniciando plotagem do gráfico...');
+
+    var ctx = canvas_temp.getContext('2d');
+    window.grafico_linha = Chart.Line(ctx, {
+        data: dados,
+        options: configurarGraficoTemp()
+    });
+
+    var ctx1 = canvas_umid.getContext('2d');
+    window.grafico_linha = Chart.Line(ctx1, {
+        data:dadosumid,
+        options:configurarGraficoUmid()
+    });
+}

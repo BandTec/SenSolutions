7576e6b18d4c529687bfd8fa96201b63ca625e90
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-28 14:32:19 -0300

    Ta cadastrando sim Meu Bom, pode testar git status!

--- SenSolutions/Site/routes/usuarios.js
+++ SenSolutions/Site/routes/usuarios.js
@@ -5,8 +5,6 @@ var banco = require('../app-banco');
 // não mexa nessas 3 linhas!
 
 
-var UsuariosModel = require('../model/UsuariosModel');
-var RespostaClass = require('../Model/RespostaClass');
 
 router.post('/entrar', function (req, res, next) {
 
@@ -40,74 +38,30 @@ router.post('/entrar', function (req, res, next) {
 
 });
 
-// router.post('/cadastrfewfwear', function (req, res, next) {
-//   console.log('chegueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeei');
-//   console.log(req.params);
-//   banco.conectar().then(() => {
-//     console.log(`Chegou p/ login: ${JSON.stringify(req.body)}`);
-//     // var login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
-//     // var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login
-//     var nomeCadastro = req.body.nomeCompleto;
-//     var emailCadastro = req.body.emailCad;
-//     var senhaCadastro = req.body.senhaCad;
-//     var senhaCadastro2 = req.body.senhaCad2;
-
-
-//     if (nomeCadastro == undefined || emailCadastro == undefined || senhaCadastro == undefined || senhaCadastro2 == undefined) {
-//       throw new Error(`Dados de cadastro não chegaram completos: ${nomeCadastro} / ${emailCadastro} / ${senhaCadastro} / ${senhaCadastro2}`);
-//     } else if (senhaCadastro != senhaCadastro2) {
-//        console.log(`Senhas incorretas! Digite a senha novamente!`);
-//     }
-//     return banco.sql.query(`Insert into tb_cliente(nomeUsuario,senhaUsuario,Email) values ('${nomeCadastro}','${senhaCadastro}','${emailCadastro}');`);
-//   }).then(consulta => {
-
-//     console.log(`Cadastro realizado com Sucesso !: ${JSON.stringify(consulta.recordset)}`);
-
-//     if (consulta.recordset.length == 1) {
-//       res.send(consulta.recordset[0]);
-//     } else {
-//       res.sendStatus(404);
-//     }
-
-//   }).catch(err => {
-
-//     var erro = `Erro no Cadastro: ${err}`;
-//     console.error(erro);
-//     res.status(500).send(erro);
-
-//   }).finally(() => {
-//     banco.sql.close();
-//   });
-
-// });
 router.post('/cadastrar', function (req, res, next) {
-  console.log('chegueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeei');
-  var nomeCadastro = req.body.nomeCompleto;
-  var emailCadastro = req.body.emailCad;
-  var senhaCadastro = req.body.senhaCad;
-  var senhaCadastro2 = req.body.senhaCad2;
+  
+  // var nomeCadastro = req.body.nomeCompleto;
+  // var emailCadastro = req.body.emailCad;
+  // var senhaCadastro = req.body.senhaCad;
+  // var senhaCadastro2 = req.body.senhaCad2;
 
 
 
   banco.conectar().then(() => {
-    if (nomeCadastro == undefined || emailCadastro == undefined || senhaCadastro == undefined || senhaCadastro2 == undefined) {
-      throw new Error(`Dados de cadastro não chegaram completos: ${nomeCadastro} / ${emailCadastro} / ${senhaCadastro} / ${senhaCadastro2}`);
-    } else if (senhaCadastro != senhaCadastro2) {
-      console.log(`Senhas incorretas! Digite a senha novamente!`);
-    }
+    // if (nomeCadastro == undefined || emailCadastro == undefined || senhaCadastro == undefined || senhaCadastro2 == undefined) {
+    //   throw new Error(`Dados de cadastro não chegaram completos: ${nomeCadastro} / ${emailCadastro} / ${senhaCadastro} / ${senhaCadastro2}`);
+    // } else if (senhaCadastro != senhaCadastro2) {
+    //   console.log(`Senhas incorretas! Digite a senha novamente!`);
+    // }
     console.log(`Chegou p/ cadastro: ${JSON.stringify(req.query)}`);
     var json = req.query;
-
     return banco.sql.query(`Insert into tb_cliente(nomeUsuario,senhaUsuario,Email) values ('${json.user}','${json.password}','${json.email}')`);
   }).then(consulta => {
-
 
     res.status(200);
     res.send('ok');
 
-
   }).catch(err => {
-
     var erro = `Erro no login: ${err}`;
     console.error(erro);
     res.status(500).send(erro);
@@ -119,7 +73,7 @@ router.post('/cadastrar', function (req, res, next) {
 
 
 router.get('/', function (req, res, next) {
-  console.log('chegueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeei');
+
 
   banco.conectar().then(() => {
     console.log(`Chegou p/ cadastro: ${JSON.stringify(req.query)}`);
@@ -143,117 +97,6 @@ router.get('/', function (req, res, next) {
     banco.sql.close();
   });
 });
-
-
-
-// router.get('/', function (req, res, next) {
-
-//     UsuariosModel.getTodos(function (erro, retorno) {
-
-//         let resposta = new RespostaClass();
-//         if (erro) {
-//             resposta.erro = true;
-//             resposta.msg = "Ocorreu um erro.";
-//             console.log("erro", erro);
-//         }
-//         else resposta.dados = retorno;
-//         res.json(resposta);
-//     });
-// });
-
-
-// router.get('/:id?', function (req, res, next) {
-
-//     UsuariosModel.getId(req.params.id, function (erro, retorno) {
-
-//         let resposta = new RespostaClass();
-
-//         if (erro) {
-//             resposta.erro = true;
-//             resposta.msg = "Ocorreu um erro.";
-//             console.log("erro", erro);
-//         }
-//         else
-//             resposta.dados = retorno;
-//         res.json(resposta);
-//     });
-// });
-
-// router.post('/', function (req, res, next) {
-
-//     UsuariosModel.adicionar(req.body, function (erro, retorno) {
-
-//         let resposta = new RespostaClass();
-
-//         if (erro) {
-//             resposta.erro = true;
-//             resposta.msg = "Ocorreu um erro.";
-//             console.log("erro", erro);
-//         }
-//         else {
-//             if (retorno.affectedRows > 0) {
-//                 resposta.msg = "cadastro realizado com sucesso";
-//             } else {
-//                 resposta.erro = true;
-//                 resposta.msg = "Não foi realziar o cadastro";
-//             }
-//         }
-//         console.log('resp cadastrar', resposta);
-//         res.json(resposta);
-//     });
-// });
-
-
-
-// router.delete('/:id', function (req, res, next) {
-
-//     UsuariosModel.deletar(req.params.id, function (erro, retorno) {
-
-//         let resposta = new RespostaClass();
-
-//         if (erro) {
-//             resposta.erro = true;
-//             resposta.msg = "Ocorreu um erro.";
-//             console.log("erro", erro);
-//         }
-//         else {
-//             if (retorno.affectedRows > 0) {
-//                 resposta.msg = "registro excluído com sucesso";
-//             } else {
-//                 resposta.erro = true;
-//                 resposta.msg = "Não foi possível excluir o registro";
-//             }
-//         }
-//         console.log('resp:', resposta);
-//         res.json(resposta);
-//     });
-// });
-
-// router.put('/', function (req, res, next) {
-
-//     UsuariosModel.editar(req.body, function (erro, retorno) {
-
-//         let resposta = new RespostaClass();
-
-//         if (erro) {
-//             resposta.erro = true;
-//             resposta.msg = "Ocorreu um erro.";
-//             console.log("erro", erro);
-//         }
-//         else {
-//             if (retorno.affectedRows > 0) {
-//                 resposta.msg = "registro alterado com sucesso";
-//             } else {
-//                 resposta.erro = true;
-//                 resposta.msg = "Não foi possível alterar o registro";
-//             }
-//         }
-//         console.log('resp:', resposta);
-//         res.json(resposta);
-//     });
-
-// });
-
 
 // não mexa nesta linha!
 module.exports = router;

1230c7e0e1f7d758029080c8f9844610d6670b90
fernandoguide <fernando-guide@hotmail.com>
2019-05-31 11:43:29 -0300

    teste

--- Banco_de_Dados/SCRIPTAZURE.sql
+++ Banco_de_Dados/SCRIPTAZURE.sql
@@ -10,7 +10,7 @@ idLocal int PRIMARY KEY identity,
 numero varchar(10),
 complemento varchar(40),
 fkCliente int,
-fkcep varchar(20),
+fkEndereco int,
 FOREIGN KEY(fkCliente) REFERENCES tb_Cliente (idCliente)
 ) go
 CREATE TABLE tb_Sensor (
@@ -24,9 +24,10 @@ Umid_Max varchar(10),
 fkLocal int
 ) go
 CREATE TABLE tb_Endereco (
-CEP varchar(20) PRIMARY KEY,
+idEndereco int PRIMARY key identity,
+CEP varchar(20) ,
 logradouro varchar(40),
-ufEstado varchar(2),
+ufEstado varchar(40),
 bairro varchar(40),
 cidade varchar(40)
 ) go
@@ -37,57 +38,9 @@ Umidade float,
 dataHora datetime,
 fkSensor int
 ) go
-/* Inserindo os dados da tabela tb_cliente */
-insert into tb_cliente(nomeUsuario,SenhaUsuario,email) values
-('Admin','admin','admin@admin.com'),
-('Admin1','admin1','admin1@admin.com'), 
-('Admin2','admin2','admin2@admin.com'), 
-('Admin3','admin3','admin3@admin.com') go 
 
--- select * from tb_clientego
 
-/* inserindo dados na tabela tb_Sensor */
-
--- delete from tb_Sensor where idSensor = 1go
-insert into tb_Sensor(SerialSensor,posicaoSensor,Temp_Max,Temp_Min,Umid_Min,Umid_Max,fkLocal) values
-('300','LADO A',35,18,30,80,1),
-('400','LADO B',35,18,30,80,2),
-('500','LADO C',35,18,30,80,3),
-('600','LADO D',35,18,30,80,1) go
-
--- select * from tb_Sensorgo
-
-/* inserindo dados na tabela  tb_Eventos*/
-
-insert into tb_Eventos(temperatura,umidade,dataHora,fksensor)values
-(25,70,current_timestamp,1),
-(24,68,current_timestamp,2),
-(27,65,'2019-04-11 02:49:00',3),
-(30,57,'2019-04-11 03:10:00',4) go
-
--- select * from tb_Eventosgo
-
-/* inserindo dados na tabela tb_Endereco */
-
-insert into tb_Endereco values
-('02805050','Rua Emilio monassa','SP','Freguesia do O',' Sao Paulo'),
-('11122236','Rua do eduardo','SP','Mooca',' Sao Paulo'),
-('12344568','Rua do Luis','SP','Casa Verde',' Sao Paulo') go
-
--- select * from tb_Enderecogo 
-
-/* inserindo dados na tabela tb_Local */
-
-insert into tb_Local(numero,complemento,fkCliente,fkCEP) values
-('188',' casa 1',1,'02805050'),
-('257',' casa 2',2,'11122236'),
-('333',' ap 57 bloco A',3,'12344568') go
-
--- select * from tb_Local
-
--- inserindo chaves estrangeiras
-
-ALTER TABLE tb_Local ADD FOREIGN KEY(fkcep) REFERENCES tb_Endereco (CEP)go
+ALTER TABLE tb_Local ADD FOREIGN KEY(fkEndereco) REFERENCES tb_Endereco (idEndereco)go
 ALTER TABLE tb_Eventos ADD FOREIGN KEY(fkSensor) REFERENCES tb_Sensor (idSensor)go
 alter table tb_Sensor add foreign key (fkLocal) references tb_Local(idLocal)go
 
